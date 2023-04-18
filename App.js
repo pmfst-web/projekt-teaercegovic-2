18ec8974e50672f195b43ece2544aa185163ae0b
teaercegovic <77160442+teaercegovic@users.noreply.github.com>
2023-04-18 21:13:51 +0200

    Izgled pocetnog ekrana

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import PocetniEkran from './screens/PocetniEkran';
+
+
+
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.ekran}>
+      <PocetniEkran />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  ekran: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
