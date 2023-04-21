7496976f391919e11dd2e52914191ec0c31cb603
Tea Ercegovic <tercegovi@pmfst.hr>
2023-04-21 12:15:53 +0200

    napravljena navigacija

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import { MaterialIcons} from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import PocetniEkran from './screens/PocetniEkran';
 import KategorijaEkran from './screens/KategorijaEkran';
@@ -11,28 +11,28 @@ import KosaraEkran from './screens/KosaraEkran';
 import ProizvodEkran from './screens/ProizvodEkran';
 
 const Stack = createNativeStackNavigator();
-//const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 import StilTekst from './constants/StilTekst';
 import Boje from './constants/Boje';
 
-/*
+
 const tabOptions = ({ route }) => ({
   headerShown: false,
   tabBarIcon: ({ focused, color, size }) => {
     let imeIkone;
-
-    if (route.name === 'Fav') {
+    if (route.name === 'Category') {
+      imeIkone = 'category';
+    } else if (route.name === 'All') {
+      imeIkone =  'view-list';
+    } else if (route.name === 'Fav') {
       imeIkone = focused ? 'favorite' : 'favorite-border';
-    } else if (route.name === 'FavPopis') {
-      imeIkone = focused ? 'favorite' : 'favorite-border';
-    }
+    } 
     return <MaterialIcons name={imeIkone} size={size} color={color} />;
   },
-  tabBarActiveTintColor: Boje.Primarna,
-  tabBarInactiveTintColor: Boje.Bijela,
+  tabBarActiveTintColor: 'black',
   tabBarStyle: {
-    backgroundColor: Boje.Naglasak1,
+    backgroundColor: Boje.istaknuto,
     height: 60
   },
 });
@@ -40,22 +40,28 @@ const tabOptions = ({ route }) => ({
 const tabEkrani = () => {
   return (
     <Tab.Navigator screenOptions={tabOptions}>
+    <Tab.Screen
+        name="Category"
+        component={KategorijaEkran}
+        initialParams={{ prikaz: 'cat' }}
+        options={{ tabBarLabel: 'Category' }}
+      />
+    <Tab.Screen
+        name="All"
+        component={KategorijaEkran}
+        initialParams={{ prikaz: 'all' }}
+        options={{ tabBarLabel: 'All' }}
+      />
       <Tab.Screen
         name="Fav"
         component={KategorijaEkran}
         initialParams={{ prikaz: 'fav' }}
-        options={{ tabBarLabel: 'Favoriti' }}
-      />
-      <Tab.Screen
-        name="Bag"
-        component={KosaraEkran}
-        initialParams={{ prikaz: 'bag' }}
-        options={{ tabBarLabel: 'Kosara' }}
+        options={{ tabBarLabel: 'Favorites' }}
       />
     </Tab.Navigator>
   );
 };
-*/
+
 
 
 export default function App() {
@@ -65,8 +71,6 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
 
-        
-      
         <Stack.Screen name="Naslovna" component={PocetniEkran} 
         options={{
               title: 'BEADS',
@@ -75,12 +79,33 @@ export default function App() {
               headerTitleStyle:  StilTekst.naslov
             }}
         />
+      
+        
 
 
 
         <Stack.Screen
           name="Kategorija"
-          component={KategorijaEkran}
+          component={tabEkrani}
+          options={({ route, navigation }) => {
+              return {
+                headerRight: () => {
+                  return (
+                    <TouchableOpacity
+                      onPress={() => navigation.navigate('Kosara')}>
+                      <View>
+                        <MaterialIcons
+                          name="shopping-cart"
+                          size={26}
+                          color='black'
+                        />
+                      </View>
+                    </TouchableOpacity>
+                  );
+                },
+              };
+            }
+          }
         />
 
         
@@ -111,11 +136,12 @@ export default function App() {
           }
         />
 
-
         <Stack.Screen
-          name="Kosara"
-          component={KosaraEkran}
-        />
+        name="Kosara"
+        component={KosaraEkran}/>
+
+
+        
 
       </Stack.Navigator>
     </NavigationContainer>
